b92afcc13a4b197417082596baa946263c238a37
FajsiEx <fajsiexofficial@gmail.com>
2019-10-27 00:27:31 +0200

    !> change to angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TouchSequence } from 'selenium-webdriver';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'edupage-calc';
+
+  subjects = ["sasa", "sasa"];
+
+  constructor() {
+  }
+
+  async ngOnInit() {
+    let tabs = await new Promise(resolve => chrome.tabs.query({ active: true, currentWindow: true }, (data)=>resolve(data)));
+    let response:any = await new Promise(resolve => chrome.tabs.sendMessage(tabs[0].id, { data: 'fetch' }, (response)=>resolve(response)));
+    this.subjects = response.data;
+    console.log(this.subjects);
+
+    setInterval(()=>{
+      console.log(this.subjects);
+    }, 5000);
+  }
 }
---
+++ src/content.js
@@ -0,0 +1,92 @@
+﻿let gradesData = [];
+
+onload = () => {
+    console.log("[ECALC] Loaded.");
+};
+
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        if (request.data == "fetch") {
+            scrapeGradeData();
+            sendResponse({ data: gradesData });
+        }
+    }
+);
+
+function scrapeGradeData() {
+    const table = document.querySelector("table");
+
+    const tableRows = table.querySelectorAll("tr:not(.header)"); // Select all rows of that table EXCEPT the header
+
+    gradeId = 0;
+    gradesData = [];
+    let subjectBuffer = {};
+    for (const row of tableRows) {
+        if (row.classList.contains("predmetRow")) {
+            // Flush the previous buffer if there is anything
+            if (Object.keys(subjectBuffer).length > 0) {
+                if (subjectBuffer.grades.length > 0) {
+                    gradesData.push(subjectBuffer);
+                    subjectBuffer = {};
+                }
+            }
+
+            let subjectName = row.querySelector("td > b").innerHTML;
+
+            if (subjectName.includes("<")) { // If there is any trace HTML left, remove it
+                subjectName = subjectName.split("<")[0];
+            }
+
+            subjectBuffer = {
+                name: subjectName,
+                grades: []
+            };
+        } else if (row.classList.contains("udalostRow")) {
+            const gradeElements = row.querySelectorAll(".znZnamka");
+
+            const rowNoteBElement = row.querySelector("td:nth-child(1) div b");
+
+            let rowWeight = 1;
+
+            if (rowNoteBElement) {
+                const rowNoteB = rowNoteBElement.innerHTML;
+
+                if (rowNoteB.includes("×")) {
+                    rowWeight = parseFloat(rowNoteB.slice(0, -1));
+                }
+            }
+
+            for (const gradeElement of gradeElements) {
+                let grade = gradeElement.innerHTML;
+
+                if (grade.includes("/")) {
+                    const reached = parseFloat(grade.split("/")[0]);
+                    const total = parseFloat(grade.split("/")[1]);
+
+                    subjectBuffer.grades.push({
+                        gradeId,
+                        type: "points",
+                        reached,
+                        total,
+                        perc: reached / total * 100,
+                        weight: rowWeight
+                    });
+                } else {
+                    subjectBuffer.grades.push({
+                        gradeId,
+                        type: "grade",
+                        grade: parseInt(grade),
+                        weight: rowWeight
+                    });
+                }
+
+                gradeId++;
+            }
+
+        } else {
+            console.warn("[ECALC] Fucc");
+        }
+    }
+
+    console.log(gradesData);
+}
